7c8b249d2e39e3ec6445abfb9a879aaa8bffadbf
0xtito <welovethepenguins@gmail.com>
2024-02-17 22:47:34 -0500

    WIP 9b: forgot this

--- packages/newtonxr/src/core/TrueHand.tsx
+++ packages/newtonxr/src/core/TrueHand.tsx
@@ -1,7 +1,8 @@
-import React, { Fragment, useEffect, useRef } from "react";
+import React, { Fragment, useEffect, useMemo, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import {
   CuboidCollider,
+  CylinderCollider,
   // CoefficientCombineRule,
   interactionGroups,
   // RapierRigidBody,
@@ -88,77 +89,133 @@ export function TrueHand({
       return;
     }
 
-    hand.updateBonesOnFrame(XRHand, xrFrame, referenceSpace);
+    hand.updateHandOnFrame(XRHand, xrFrame, referenceSpace);
   });
 
-  // return (
-  //   <Fragment key={id}>
-  //     {hand.completeFingers.map((fingerBones) => {
-  //       return (
-  //         <>
-  //           {fingerBones.map((bone) => (
-  //             <Fragment key={`kinematicBone: ` + bone.id}>
-  //               <RigidBody
-  //                 ref={bone.refs.kinematicBoneRef}
-  //                 type="kinematicPosition"
-  //                 colliders={false}
-  //                 canSleep={false}
-  //                 collisionGroups={interactionGroups([], [])}
-  //               >
-  //                 <mesh visible={false}>
-  //                   <boxGeometry
-  //                     args={[
-  //                       bone.boxArgs.width,
-  //                       bone.boxArgs.height,
-  //                       bone.boxArgs.depth,
-  //                     ]}
-  //                   />
-  //                   <meshBasicMaterial wireframe color="black" />
-  //                 </mesh>
-  //               </RigidBody>
-  //             </Fragment>
-  //           ))}
-
-  //           <RigidBody
-  //             type="dynamic"
-  //             gravityScale={0}
-  //             restitution={0}
-  //             // restitutionCombineRule={CoefficientCombineRule.Min}
-  //             // friction={0}
-  //             canSleep={false}
-  //             // colliders="cuboid"
-  //             colliders={false}
-  //             // interacts with objects and TrackedMeshes
-  //             collisionGroups={interactionGroups([0], [7, 8])}
-  //             density={5}
-  //             // dominanceGroup={5}
-  //             onCollisionEnter={(payload) => {
-  //               const { target } = payload;
-  //               console.log("bone collision enter ", payload);
-  //               target.rigidBody?.lockRotations(true, true);
-  //               // target.rigidBody?.lockTranslations(true, true);
-  //             }}
-  //             onCollisionExit={(payload) => {
-  //               const { target } = payload;
-  //               // console.log("bone collision exit ", payload);
-  //               target.rigidBody?.lockRotations(false, true);
-
-  //               // target.rigidBody?.lockTranslations(false, true);
-  //             }}
-  //             ccd
-  //             {...props}
-  //           >
-  //             {fingerBones.map((bone) => (
-  //               <>
-  //                 <RoundCylinderCollider></RoundCylinderCollider>
-  //               </>
-  //             ))}
-  //           </RigidBody>
-  //         </>
-  //       );
-  //     })}
-  //   </Fragment>
+  // console.log("\n---------- TrueHand -------------");
+  // console.log("hand.bones: ", hand.bones);
+  // console.log("hand.trueHandBones: ", hand.trueHandBones);
+  // console.log("hand.trueHandJoints: ", hand.trueHandJoints);
+  // console.log(
+  //   "hand.kinematicWrist.current?.translation(): ",
+  //   hand.kinematicWrist.current?.translation(),
   // );
+  // console.log("---------------------------------\n-");
+
+  return (
+    <Fragment key={id}>
+      {/* KinematicHand Wrist Rigid Body */}
+      <RigidBody
+        key="kinematicWrist"
+        type="kinematicPosition"
+        ref={hand.kinematicWrist}
+        colliders={false}
+        canSleep={false}
+      >
+        {/* <mesh visible={false}>
+          <boxGeometry args={[0.05, 0.05, 0.05]} />
+          <meshBasicMaterial wireframe color="black" />
+        </mesh> */}
+      </RigidBody>
+
+      {/* TrueHand Rigid Body Joints */}
+      {hand.trueHandJoints.map(([name, jointInfo]) => {
+        return (
+          <>
+            <RigidBody
+              key={name + "trueHandJoint"}
+              type="dynamic"
+              gravityScale={0}
+              restitution={0}
+              canSleep={false}
+              ref={jointInfo.rigidBody}
+              colliders={false}
+            >
+              <mesh>
+                <sphereGeometry args={[0.001]} />
+                <meshBasicMaterial color="red" />
+              </mesh>
+            </RigidBody>
+          </>
+        );
+      })}
+
+      {/* TrueHand Rigid Body Bones */}
+      {hand.trueHandBones.map((bone) => {
+        if (!bone.boxArgs.height) return null;
+        return (
+          <RigidBody
+            key={bone.id}
+            type="dynamic"
+            gravityScale={0}
+            canSleep={false}
+            colliders={false}
+            userData={{ name: bone.name }}
+            collisionGroups={interactionGroups([0], [7, 8])}
+            density={5}
+            onCollisionEnter={(payload) => {
+              const { target } = payload;
+              console.log("bone collision enter ", payload);
+              target.rigidBody?.lockRotations(true, true);
+            }}
+            onCollisionExit={(payload) => {
+              const { target } = payload;
+              target.rigidBody?.lockRotations(false, true);
+            }}
+            ccd
+            {...props}
+            ref={bone.refs.trueBoneRef}
+          >
+            <mesh visible={true}>
+              {boneShape === "cylinder" ? (
+                <>
+                  <cylinderGeometry
+                    args={[
+                      bone.boxArgs.width / 2,
+                      bone.boxArgs.width / 2,
+                      bone.boxArgs.height,
+                    ]}
+                  />
+
+                  <RoundCylinderCollider
+                    args={[bone.boxArgs.height / 2, bone.boxArgs.width / 2, 0]}
+                    friction={1}
+                    restitution={0}
+                  />
+                </>
+              ) : (
+                <>
+                  <boxGeometry
+                    args={[
+                      bone.boxArgs.width,
+                      bone.boxArgs.height,
+                      bone.boxArgs.depth,
+                    ]}
+                  />
+
+                  <CuboidCollider
+                    args={[
+                      bone.boxArgs.width,
+                      bone.boxArgs.height,
+                      bone.boxArgs.depth,
+                    ]}
+                    friction={1}
+                    restitution={0}
+                  />
+                </>
+              )}
+
+              <meshBasicMaterial
+                transparent
+                opacity={bonesVisible ? 0.5 : 0}
+                color="white"
+              />
+            </mesh>
+          </RigidBody>
+        );
+      })}
+    </Fragment>
+  );
 
   return (
     <Fragment key={id}>
@@ -260,6 +317,96 @@ export function TrueHand({
             </RigidBody>
           </Fragment>
         );
+      })}
+    </Fragment>
+  );
+
+  return (
+    <Fragment key={id}>
+      {hand.completeFingerBones.map(([name, fingerBones]) => {
+        if (!fingerBones.bones[0]?.boxArgs.height) return null;
+
+        // console.log("\n---------- fingerBones -------------");
+        // console.log("name: ", name);
+        // console.log(
+        //   "kinematicFinger translation",
+        //   fingerBones.fingerRefs.kinematicFinger.current?.translation(),
+        // );
+        // console.log(
+        //   "kinematicFinger rotation",
+        //   fingerBones.fingerRefs.kinematicFinger.current?.rotation(),
+        // );
+
+        if (fingerBones.bones[0]?.refs.trueBoneColliderRef?.current)
+          return (
+            <RigidBody
+              key={name}
+              type="kinematicPosition"
+              ref={fingerBones.fingerRefs.kinematicFinger}
+              // colliders={false}
+            >
+              {fingerBones.bones.map((bone) => {
+                if (!bone.boxArgs.height) return null;
+
+                console.log("\n----------------- bone -----------------");
+                console.log("bone: ", bone);
+                console.log("bone.transform", bone.transform);
+                console.log(
+                  "bone.refs.trueBoneColliderRef translation",
+                  bone.refs.trueBoneColliderRef?.current?.translation(),
+                );
+                console.log(
+                  "bone.refs.trueBoneColliderRef rotation",
+                  bone.refs.trueBoneColliderRef?.current?.rotation(),
+                );
+                console.log("--------------------------------------\n");
+
+                // bone.refs.trueBoneColliderRef?.current?.setTranslation(
+                //   bone.transform.position,
+                // );
+
+                return (
+                  <RoundCylinderCollider
+                    key={bone.name}
+                    args={[bone.boxArgs.height / 2, bone.boxArgs.width / 2, 0]}
+                    friction={1}
+                    restitution={0}
+                    ref={bone.refs.trueBoneColliderRef}
+                  >
+                    <mesh>
+                      <cylinderGeometry
+                        args={[
+                          bone.boxArgs.width / 2,
+                          bone.boxArgs.width / 2,
+                          bone.boxArgs.height,
+                        ]}
+                      />
+                      <meshBasicMaterial
+                        transparent
+                        opacity={0.5}
+                        color="white"
+                      />
+                    </mesh>
+                  </RoundCylinderCollider>
+
+                  // <mesh
+                  //   key={bone.name}
+                  //   position={bone.transform.position}
+                  //   quaternion={bone.transform.orientation}
+                  // >
+                  //   <boxGeometry
+                  //     args={[
+                  //       bone.boxArgs.width,
+                  //       bone.boxArgs.height,
+                  //       bone.boxArgs.depth,
+                  //     ]}
+                  //   />
+                  //   <meshBasicMaterial wireframe color="black" />
+                  // </mesh>
+                );
+              })}
+            </RigidBody>
+          );
       })}
     </Fragment>
   );
