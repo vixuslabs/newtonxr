0a332d03d3a699e269a472dcf5bd19980fd4cad9
0xtito <92818759+0xtito@users.noreply.github.com>
2024-11-05 11:05:46 +0800

    chore: proper exports

--- packages/newtonxr/src/index.ts
+++ packages/newtonxr/src/index.ts
@@ -1,8 +1,5 @@
 export * from "./BuildPhysicalMeshes.js";
 export * from "./BuildPhysicalPlanes.js";
-// export * from "./Grab.js";
-export * from "./PhysicalController.js";
-export * from "./SpatialHand.js";
 
 export * from "./core/index.js";
 export * from "./objects/index.js";
